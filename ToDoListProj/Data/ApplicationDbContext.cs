93cb767ed94ccd36d5bfd07f701f2e3175ea82a5
Maks <babyshigaraki@gmail.com>
2025-03-05 22:10:59 +0100

    AddingSQLLITE

--- ToDoListProj/Data/ApplicationDbContext.cs
+++ ToDoListProj/Data/ApplicationDbContext.cs
@@ -23,34 +23,37 @@ namespace ToDoListProj.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-
-            // Визначення зв'язків між сутностями
-            modelBuilder.Entity<GroupMember>()
-                .HasKey(gm => new { gm.GroupTaskId, gm.UserId }); // Задаємо комбінований ключ для GroupMember
+            if (Database.ProviderName == "Microsoft.EntityFrameworkCore.Sqlite")
+            {
+                modelBuilder.Entity<ApplicationUser>().Property(u => u.UserName).HasMaxLength(256);
+                modelBuilder.Entity<ApplicationUser>().Property(u => u.Email).HasMaxLength(256);
+            }
+             modelBuilder.Entity<GroupMember>()
+                .HasKey(gm => new { gm.GroupTaskId, gm.UserId }); 
 
             modelBuilder.Entity<GroupMember>()
                 .HasOne(gm => gm.User)
-                .WithMany(u => u.GroupMemberships) // Це залежить від того, чи має ApplicationUser колекцію GroupMemberships
+                .WithMany(u => u.GroupMemberships)  
                 .HasForeignKey(gm => gm.UserId);
 
             modelBuilder.Entity<GroupMember>()
                 .HasOne(gm => gm.GroupTask)
-                .WithMany(gt => gt.Members) // Колекція членів у GroupTask
-                .HasForeignKey(gm => gm.GroupTaskId); // Виправлення: правильне ім'я зовнішнього ключа
+                .WithMany(gt => gt.Members)  
+                .HasForeignKey(gm => gm.GroupTaskId); 
 
             modelBuilder.Entity<Friendship>()
-                .HasKey(f => f.Id); // визначення первинного ключа
+                .HasKey(f => f.Id); 
 
             modelBuilder.Entity<Friendship>()
-                .HasOne(f => f.User) // Зв'язок з ApplicationUser (перший користувач)
-                .WithMany() // Користувач може мати багато друзів
-                .HasForeignKey(f => f.UserId) // Зовнішній ключ для UserId
-                .OnDelete(DeleteBehavior.Restrict); // При видаленні користувача не видаляти дружбу
+                .HasOne(f => f.User)  
+                .WithMany() 
+                .HasForeignKey(f => f.UserId) 
+                .OnDelete(DeleteBehavior.Restrict);  
 
             modelBuilder.Entity<Friendship>()
-                .HasOne(f => f.Friend) // Зв'язок з ApplicationUser (другий користувач)
-                .WithMany() // Друг може мати багато користувачів
-                .HasForeignKey(f => f.FriendId) // Зовнішній ключ для FriendId
+                .HasOne(f => f.Friend)  
+                .WithMany()  
+                .HasForeignKey(f => f.FriendId)  
                 .OnDelete(DeleteBehavior.Restrict);
         }
     }
---
+++ ToDoListProj/Migrations/20250301193447_AddingImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GroupImageUrl",
+                table: "GroupTasks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GroupImageUrl",
+                table: "GroupTasks");
+        }
+    }
+}
